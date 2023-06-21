228a55eb04332c1879533d39554639e26fcbb754
gervajac <gerva.jacob.tropini@gmail.com>
2023-06-21 11:04:32 -0300

    popupped

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,28 @@
 import React from 'react';
 import {
-    ThemeProvider,
     DarkTheme,
     DefaultTheme,
     useTheme,
   } from "@react-navigation/native";
 import { Slot } from 'expo-router';
+import { ThemeProvider } from "@react-navigation/native";
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const App = () => {
+
+    const auth0Config = {
+        clientId: 'U9eZhNFhLemhM7cN42ijlTSqkTG3pKj4',
+        domain: 'dev-o3oiqmf8nwbj4qxk.us.auth0.com',
+        redirectUri: 'http://localhost:19000',
+      };
+
+
     return (
+        <Auth0Provider {...auth0Config}>
         <ThemeProvider value={DarkTheme}>
             <Slot />
         </ThemeProvider>
+        </Auth0Provider>
     );
 };
 
